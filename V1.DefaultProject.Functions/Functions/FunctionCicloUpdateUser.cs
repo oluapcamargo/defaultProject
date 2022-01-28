b85b1afb1c7b2ebd6a4352557cedd03c3d37bbfa
Paulo Henrique Camargo <oluapcamargo@gmail.com>
2022-01-28 12:11:15 -0300

    Add project Function;

---
+++ V1.DefaultProject.Functions/Functions/FunctionCicloUpdateUser.cs
@@ -0,0 +1,40 @@
+﻿using Confluent.Kafka;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using V1.DefaultProject.Domain.Entities;
+using V1.DefaultProject.Domain.Interfaces;
+
+namespace V1.DefaultProject.Functions.Functions
+{
+    public class FunctionCicloUpdateUser
+    {
+        private readonly IUsuarioRepository _homeRepositorio;
+
+        public FunctionCicloUpdateUser(IUsuarioRepository homeRepositorio)
+        {
+            _homeRepositorio = homeRepositorio;
+        }
+        [FunctionName("FunctionCicloUpdateUser")]
+        public async Task Run([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer, ILogger log)
+        {
+            var usuarios = (await _homeRepositorio.GetAllAsync()).ToList();
+            if (usuarios == null)
+            {
+                usuarios = new List<Usuario>();
+                usuarios.Add(new Usuario("Usuario Ciclo Update", "11111111111", "teste@tteste.com.br"));
+
+                foreach (var item in usuarios)
+                {
+                    item.Nome += " - Function Ciclo" + DateTime.Now.ToShortDateString();
+                    await _homeRepositorio.UpdateAsync(item);
+                }
+            }
+        }
+    }
+}
---
+++ V1.DefaultProject.Functions/Functions/FunctionQueueCicloUpdateUser.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using V1.DefaultProject.Domain.Interfaces;
+
+namespace V1.DefaultProject.Functions.Functions
+{
+    public class FunctionQueueCicloUpdateUser
+    {
+        private readonly IUsuarioRepository _homeRepositorio;
+
+        public FunctionQueueCicloUpdateUser(IUsuarioRepository homeRepositorio)
+        {
+            _homeRepositorio = homeRepositorio;
+        }
+        [FunctionName("FunctionCicloUpdateUser")]
+        public async Task Run([QueueTrigger("queue-update-user", Connection = "StorageConnectionString")] Guid input)
+        {
+            var usuario = await _homeRepositorio.GetAsync(input);
+            usuario.Nome += " - Function Queue" + DateTime.Now.ToShortDateString();
+            await _homeRepositorio.UpdateAsync(usuario);
+        }
+    }
+}
